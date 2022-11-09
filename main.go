391d9887aeb06be2207cd8be4ae1b36dc8d66966
artempaskov <pashkovchik@gmail.com>
2022-11-09 16:53:57 +0300

    home page add

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Go is super easy!")
+}
 
 func main() {
 	http.HandleFunc("/", home_page)
 	http.ListenAndServe(":8080", nil)
-	
+
 }
