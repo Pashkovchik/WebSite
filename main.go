81a29e6a8828fed31c19af635285681094012983
artempaskov <pashkovchik@gmail.com>
2022-11-09 23:56:46 +0300

    style

--- main.go
+++ main.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type User struct {
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
+	Hobbies               []string
+}
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has "+
+		"money equal: %d", u.Name, u.Age, u.Money)
+}
+
+func (u *User) setNewName(newName string) {
+	u.Name = newName
+}
+
 func home_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Go is super easy!")
+	bob := User{"Artem", 31, 50, 4.2, 0.8, []string{"Football", "Skate", "Dance"}}
+	//bob.setNewName("Pashkovchik")
+	//fmt.Fprint(w, `<h1>Main Text</h1>
+	//<b>Main Text</b>`)
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
@@ -20,5 +43,6 @@ func handleRequest() {
 }
 
 func main() {
+	// bob := User{name: "Bob", age: 25, money: -50, avg_grades: 4.2, happiness: 0.8}
 	handleRequest()
 }
